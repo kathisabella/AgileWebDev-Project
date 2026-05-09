159f68f5734d634754c0b0e3c8326d601c9ff0ab
Kathleen <kathleen.isabella01@gmail.com>
2026-05-10 01:28:16 +0800

    Set up Flask app with dotenv and SQLAlchemy
    Initialize Flask app with environment variables and database.

--- main/__init__.py
+++ main/__init__.py
@@ -1,1 +1,18 @@
+from dotenv import load_dotenv
+load_dotenv()
 
+from flask import Flask
+from flask import Config
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy 
+
+from main.config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migration = Migrate(app, db)
+
+from main import routes
+## from main import models
