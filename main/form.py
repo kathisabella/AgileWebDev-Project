e2b08c494021249266eb3f2c0a7db1db8fa10779
Brandonfzh <brandonfongzhenhang30@gmail.com>
2026-05-09 10:30:45 +0800

    Added registerform for flaskwtform as well

--- main/form.py
+++ main/form.py
@@ -1,10 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, IntegerField, BooleanField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, Equalto
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     saved_login = BooleanField('Remember me')
-    login = SubmitField('login')
+    login = SubmitField('Log in')
 
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), Equalto('password')])
+    sign_up = SubmitField('Sign Up')
