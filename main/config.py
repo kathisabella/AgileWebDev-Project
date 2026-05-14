9f98a67f7ba6c0834ee867a5ebf768f37e47eb44
Brandonfzh <brandonfongzhenhang30@gmail.com>
2026-05-14 17:22:48 +0800

    Refactor config.py and make multiple classes for the actual config and the test config

--- main/config.py
+++ main/config.py
@@ -1,11 +1,17 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-default_db_path = 'sqlite:///' + os.path.join(basedir, 'plateful.db')
 
 class Config: 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('APP_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('MYAPP_DATABASE_URL') or default_db_path
+
+class DeploymentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('MYAPP_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'plateful.db')
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    TESTING = True
+    WTF_CSRF_ENABLED = False
