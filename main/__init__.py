f61cbf485bc5f418d239707fecd29f50db74f266
Brandonfzh <brandonfongzhenhang30@gmail.com>
2026-05-14 17:23:32 +0800

    Refactor __init__ for factory pattern and test support

--- main/__init__.py
+++ main/__init__.py
@@ -1,16 +1,16 @@
-from flask import Flask
-from flask import Config
 from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy 
-from flask_wtf.csrf import CSRFProtect
-from main.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
-app = Flask(__name__)
-app.config.from_object(Config)
+db = SQLAlchemy()
 
-db = SQLAlchemy(app)
-migration = Migrate(app, db)
-csrf = CSRFProtect(app)
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
 
-from main import routes
-from main import models
+    db.init_app(app)
+
+    from main.blueprints import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
